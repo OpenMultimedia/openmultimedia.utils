fe8d93ac23cdf83b03a76e4dddf0a88f6688ea32
Gonzalo Almeida <flecox@gmail.com>
2012-10-30 12:18:05 -0300

    agregando footer faltan pruebas

--- src/openmultimedia/utils/testing.py
+++ src/openmultimedia/utils/testing.py
@@ -35,3 +35,28 @@ FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(FIXTURE,),
     name='openmultimedia.utils:Functional',
     )
+
+def browserLogin(portal, browser, username=None, password=None):
+    handleErrors = browser.handleErrors
+    try:
+        browser.handleErrors = False
+        browser.open(portal.absolute_url() + '/login_form')
+        if username is None:
+            username = TEST_USER_NAME
+        if password is None:
+            password = TEST_USER_PASSWORD
+        browser.getControl(name='__ac_name').value = username
+        browser.getControl(name='__ac_password').value = password
+        browser.getControl(name='submit').click()
+    finally:
+        browser.handleErrors = handleErrors
+
+
+def createObject(context, _type, id, delete_first=False,
+                 check_for_first=False, **kwargs):
+    if delete_first and id in context.objectIds():
+        context.manage_delObjects([id])
+    if not check_for_first or id not in context.objectIds():
+        return context[context.invokeFactory(_type, id, **kwargs)]
+
+    return context[id]
